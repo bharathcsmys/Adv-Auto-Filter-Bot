8fadba79f3ccf7845b9b875702d7dadded6f4661
bharathcsmys <79088828+bharathcsmys@users.noreply.github.com>
2021-03-27 18:33:14 +0530

    Translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -5,7 +5,7 @@
 class Translation(object):
     
     START_TEXT = """<b>Hai {}!!</b>
-<i>Am Just A Simple Hand Auto Filter Bot_ Bot For Searching Files From Channel...
+<i>Am Just A Simple Hand Auto Filter Bot_ Bot For Searching Files From Channel...This is my master > @SINCE_14C5 
 
 Just Sent Any Text I Will Search In All Connected Chat And Reply You With The Message link
 
@@ -30,11 +30,11 @@ Just Send Any Text Will Try To Lookup In Channel And Provide You The Link</i>
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code>Adv Auto Filter Bot</code>
 
-<b>➥ Creator</b> : <b><i><a herf="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a herf="https://t.me/SINCE_14C5">AlbertEinstein_TG</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a herf="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0</a></i>
 
-<b>➥ Source Code</b> : <i><a herf="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">GitHub</a></i>
+<b>➥ Source Code</b> : <i><a herf = "t.me/ELEVENO_ELDORADO">GitHub</a></i>
 """
